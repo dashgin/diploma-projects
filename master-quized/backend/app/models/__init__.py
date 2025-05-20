30e0e51c7bc448fa78b26dc28ee9b64b86dd9e2b
Dashgin <76277687+dashgin@users.noreply.github.com>
2025-05-20 22:38:06 +0400

    Refactor backend rules and remove unused models. Updated backend.mdc to enhance coding guidelines and removed models.py as it is no longer needed.

---
+++ master-quized/backend/app/models/__init__.py
@@ -0,0 +1,21 @@
+from .user import User, UserCreate, UserUpdate, UserPublic, UsersPublic, UpdatePassword, UserRegister
+from .item import Item, ItemCreate, ItemUpdate, ItemPublic, ItemsPublic
+from .common import Message, Token, TokenPayload
+
+__all__ = [
+    "User", 
+    "UserCreate", 
+    "UserUpdate", 
+    "UserPublic", 
+    "UsersPublic", 
+    "UpdatePassword", 
+    "UserRegister", 
+    "Item", 
+    "ItemCreate", 
+    "ItemUpdate", 
+    "ItemPublic", 
+    "ItemsPublic", 
+    "Message", 
+    "Token", 
+    "TokenPayload"
+]
